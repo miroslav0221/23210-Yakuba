0524fb98482a7573b7899a9fc0a5ed5fe7d99c6f
Мирослав Якуба <miroslav@MacBook-Air-Miroslav.local>
2024-12-25 21:48:50 +0700

    delete optional comments

--- lab4/include/tuple_printer.h
+++ lab4/include/tuple_printer.h
@@ -1,73 +1,3 @@
-// #ifndef TUPLE_H
-// #define TUPLE_H
-// #include <fstream>
-// #include <sstream>
-// #include <string>
-// #include <tuple>
-// #include <vector>
-//
-//
-// template <typename Tuple, size_t Index>
-// struct TuplePrinter {
-//     static void print_tuple(const Tuple& tp, std::ostream& os) {
-//         TuplePrinter<Tuple, Index - 1>::print_tuple(tp, os); // Рекурсивный вызов
-//         os << ", " << std::get<Index>(tp);
-//     }
-// };
-//
-// template <typename Tuple>
-// struct TuplePrinter<Tuple, 0> {
-//     static void print_tuple(const Tuple& tp, std::ostream& os) {
-//         os << std::get<0>(tp);
-//     }
-// };
-//
-// template <typename Ch, typename Tr, typename... Args>
-// std::basic_ostream<Ch, Tr>& operator<<(std::basic_ostream<Ch, Tr>& os, const std::tuple<Args...>& t) {
-//     os << "(";
-//     if (sizeof...(Args) > 0) {
-//         TuplePrinter<std::tuple<Args...>, sizeof...(Args) - 1>::print_tuple(t, os);
-//     }
-//     os << ")";
-//     return os;
-// }
-//
-// template <typename T>
-// T type_conversion(const std::string& str) {
-//     std::stringstream tmp(str);
-//     T value;
-//     if(!(tmp >> value)) {
-//         throw std::invalid_argument("Convert str error");
-//     }
-//     return value;
-// }
-//
-// template <typename T, typename... Rest>
-// std::tuple<T, Rest...> create_tuple_recursive(const std::vector<std::string>& vec, size_t index = 0) {
-//     try {
-//         T value = type_conversion<T>(vec[index]);
-//     }
-//     catch (std::invalid_argument& er) {
-//         std::ostringstream error_mes;
-//         error_mes << "Error at col: " << index + 1;
-//         throw std::invalid_argument(error_mes.str());
-//     }
-//
-//     T value = type_conversion<T>(vec[index]);
-//     if constexpr (sizeof...(Rest) == 0) {
-//         return std::make_tuple(value);
-//     } else {
-//         return std::tuple_cat(std::make_tuple(value), create_tuple_recursive<Rest...>(vec, index + 1));
-//     }
-// }
-//
-// template <typename... Args>
-// std::tuple<Args...> create_tuple(const std::vector<std::string>& vec) {
-//     return create_tuple_recursive<Args...>(vec);
-// }
-//
-// #endif
-//
 #ifndef TUPLE_H
 #define TUPLE_H
 
@@ -80,7 +10,7 @@
 template <typename Tuple, size_t Index>
 struct TuplePrinter {
     static void print(const Tuple& tp, std::ostream& os) {
-        TuplePrinter<Tuple, Index - 1>::print(tp, os); // Рекурсивный вызов для предыдущих элементов
+        TuplePrinter<Tuple, Index - 1>::print(tp, os);
         os << ", " << std::get<Index>(tp);
     }
 };
